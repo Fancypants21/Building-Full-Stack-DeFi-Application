ea6df5058f0f3a7b2ecadb389244ecfd51d767c9
Corey Magin <thefancypants21@gmail.com>
2024-11-22 14:02:04 -0800

    Update Utils.js

--- defi-apps/src/backend/test/Utils.js
+++ defi-apps/src/backend/test/Utils.js
@@ -1,6 +1,6 @@
 const { ethers } = require("hardhat");
-const toWei = (num) => ethers.utils.parseEther(num.toString());
-const fromWei = (num) => ethers.utils.formatEther(num);
+const toWei = (num) => ethers.parseEther(num.toString());
+const fromWei = (num) => ethers.formatEther(num);
 const delay = ms => new Promise(res => setTimeout(res, ms));
 exports.toWei = toWei;
 exports.fromWei = fromWei;
